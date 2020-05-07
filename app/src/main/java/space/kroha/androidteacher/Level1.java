21fbe141291b195c39af269127e2b1d51acf66f1
Дмитрий <dimkakrohin@mail.ru>
2020-05-07 22:17:05 +0300

    data received and installed

--- app/src/main/java/space/kroha/androidteacher/Level1.java
+++ app/src/main/java/space/kroha/androidteacher/Level1.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -14,62 +15,87 @@ import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-public class Level1 extends AppCompatActivity {
-    private FirebaseFirestore db;
-    List<Lesson> list = new ArrayList<>();
-    Button button1;
+public class Level1 extends AppCompatActivity  {
+
+    Button button2;
+    Button button3;
+    Button button4;
+    ImageView img;
+    int questionNumber;
+    private static FirebaseFirestore db;
+    public static List<Lesson> list;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
-        button1 = findViewById(R.id.button2);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        img = findViewById(R.id.imageTest);
 
-
-        final ImageView img = findViewById(R.id.imageTest);
-
-        //код который скругляет углы картинки
+        list = new ArrayList<>();
         img.setClipToOutline(true);
 
-        //Убираем все менюшки с экрана начало
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //Убираем все менюшки с экрана конец
 
+        list = new ArrayList<>();
+        questionNumber = 0;
+
+        setButtonText(questionNumber);
+    }
+
+
+
+    private void readData(final FirestoreCallback firestoreCallback){
         db = FirebaseFirestore.getInstance();
-                db.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()){
-                            Log.i("123233333", "YES!");
-                            QuerySnapshot querySnapshot = task.getResult();
-                            if (querySnapshot == null)return;
-                            for (QueryDocumentSnapshot documentSnapshot : querySnapshot){
-                                Lesson lesson = documentSnapshot.toObject(Lesson.class);
-                                list.add(lesson);
-                            }
-                        }else {
-                            Log.i("123233333", "NO!");
-                        }
-                        Log.i("333", list.get(2).getAge());
-                        button1.setText(list.get(2).getAge());
+        db.collection("users")
+              .get()
+              .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                  @Override
+                  public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                      if (task.isSuccessful()){
+                          Log.i("123233333", "Данные есть!");
+                          QuerySnapshot querySnapshot = task.getResult();
+                          if (querySnapshot == null)return;
+                          for (QueryDocumentSnapshot documentSnapshot : querySnapshot){
+                              Lesson lesson = documentSnapshot.toObject(Lesson.class);
+                              list.add(lesson);
+                          }
+                          firestoreCallback.onCallback(list);
+                      }else {
+                          Log.i("123233333", "НЕТ ДАННЫХ");
+                      }
+                      Log.i("333", list.get(1).getAge());
+                  }
+              });
+    }
 
+
+    private interface FirestoreCallback{
+        void onCallback(List<Lesson> list);
+    }
+
+    public void setButtonText (final int a){
+        readData(new FirestoreCallback() {
+            @Override
+            public void onCallback(List<Lesson> list) {
+                button2.setText(list.get(a).getName());
+                button3.setText(list.get(a + 1).getName());
+                button4.setText(list.get(a + 2).getName());
+                System.out.println(a);
 
             }
-
         });
-
-
-
     }
+
 }
